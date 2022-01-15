0d713f7763704de3cb55c182841c658bc409c782
Kelsey Erickson <kerickson@gmx.com>
2022-01-15 22:35:42 +0000

    Add flash message if login not completed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to "/"
     else 
+      flash[:errors] = "Incorrect email or password"
       redirect_to "/login"
     end
   end
