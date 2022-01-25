a8e4845220427feebfef86ccbd8f42f45ef72f52
Kelsey Erickson <kerickson@gmx.com>
2022-01-25 18:34:41 +0000

    Add minimum length for user password

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
 
   has_secure_password
   validates :first_name, :last_name, :email, :password, :password_confirmation, :presence => true
+  validates :password, length: { minimum: 8 }, :presence => true
   validates_uniqueness_of :email, :case_sensitive => false
   
 end
