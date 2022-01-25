c23ef65eec8ac2ecd3df8d1e45ab6af55c7a61c5
Kelsey Erickson <kerickson@gmx.com>
2022-01-25 18:26:56 +0000

    Add examples for user validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,11 +15,36 @@ RSpec.describe User, type: :model do
       expect(@user.errors.full_messages).to include("Password confirmation can't be blank") 
     end
 
-
     it "will not validate without if password and password confirmation are not the same" do
       @user = User.new(first_name:"name", last_name:"last_name", email:"sample@email.com", password: "password1234", password_confirmation:"lemongrab1234")
       @user.validate
       expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password") 
+    end
+
+    it "will not validate if email has been registered" do
+      @user = User.new(first_name:"name", last_name:"last_name", email:"sample@email.com", password: "password1234", password_confirmation: "password1234")
+      @user.save!
+      @user2 = User.new(first_name:"name", last_name:"last_name", email:"SAMPLE@email.com", password: "password1234", password_confirmation: "password1234")
+      @user2.validate
+      expect(@user2.errors.full_messages).to include("Email has already been taken") 
+    end
+
+    it "will not validate without an email" do
+      @user = User.new(first_name:"name", last_name:"last_name", email: nil, password: "password1234", password_confirmation: "password1234")
+      @user.validate
+      expect(@user.errors.full_messages).to include("Email can't be blank") 
+    end
+
+    it "will not validate without a first name" do
+      @user = User.new(first_name: nil, last_name:"last_name", email: "sample@email.com", password: "password1234", password_confirmation: "password1234")
+      @user.validate
+      expect(@user.errors.full_messages).to include("First name can't be blank") 
+    end
+
+    it "will not validate without a last name" do
+      @user = User.new(first_name:"name", last_name:nil, email: "sample@email.com", password: "password1234", password_confirmation: "password1234")
+      @user.validate
+      expect(@user.errors.full_messages).to include("Last name can't be blank") 
     end
   end
 end
