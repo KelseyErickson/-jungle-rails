b13f62c1278b4cd3d0a166afc427c3ae97b7e21d
Kelsey Erickson <kerickson@gmx.com>
2022-01-25 18:26:18 +0000

    Add that email uniqueness is not case sensitive

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ActiveRecord::Base
 
   has_secure_password
   validates :first_name, :last_name, :email, :password, :password_confirmation, :presence => true
-  validates_uniqueness_of :email
+  validates_uniqueness_of :email, :case_sensitive => false
   
 end
